7ec0d86bb2209a2a55b72a29e037706bea1ade1d
h3nryfost3r <henry.foster71@yahoo.com>
2023-10-27 16:33:34 +0300

    added config and set up alembic cofiguration

--- src/app.py
+++ src/app.py
@@ -1,14 +1,18 @@
-from typing import Dict
+from typing import Annotated, Dict
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.v1.router import router as router_v1
+from src.database import get_async_session
 
 app = FastAPI()
 
 
-@app.get("/ping", tags=["Ping"])
-async def ping() -> Dict[str, str]:
+@app.get("/ping", tags=["Ping"], status_code=status.HTTP_200_OK)
+async def ping(
+    session: Annotated[AsyncSession, Depends(get_async_session)],
+) -> Dict[str, str]:
     return {"ping": "ok"}
 
 
